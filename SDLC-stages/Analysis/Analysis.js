92c53d2cf872bdf2b859d3b26263b552f041bf92
Rayan <Rayanelfayoumi@gmail.com>
2024-06-12 11:06:39 -0400

    Added project

---
+++ SDLC-stages/Analysis/Analysis.js
@@ -0,0 +1,6 @@
+function buttonFuction() {
+    document.getElementById('demo').innerHTML = 'Correct!'
+}
+function dblButtonFunction() {
+    document.getElementById('dblClick').innerHTML = 'Theyre all correct!'
+}
